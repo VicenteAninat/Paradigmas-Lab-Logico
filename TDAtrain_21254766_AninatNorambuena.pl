2b1a6e5d675d53460b9e74e81d7928c2617de52e
Vicente Aninat <vicente.aninat@usach.cl>
2024-05-13 21:39:57 -0400

    Se añadieron los archivos referentes a los TDA conductor y tren

---
+++ TDAtrain_21254766_AninatNorambuena.pl
@@ -0,0 +1,5 @@
+:- consult("TDAstation_21254766_AninatNorambuena.pl").
+:- consult("TDAsection_21254766_AninatNorambuena.pl").
+:- consult("TDAline_21254766_AninatNorambuena.pl").
+:- consult("TDApcar_21254766_AninatNorambuena.pl").
+%TDA train

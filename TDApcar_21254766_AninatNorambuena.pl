4bd039e34321d669b614bc74c4e4f1fa1a3e4694
Vicente Aninat <vicente.aninat@usach.cl>
2024-05-15 21:18:07 -0400

    Se modificó la base de conocimientos dentro del TDA referente a los carros de metro

--- TDApcar_21254766_AninatNorambuena.pl
+++ TDApcar_21254766_AninatNorambuena.pl
@@ -1,4 +1,21 @@
-:- consult("TDAstation_21254766_AninatNorambuena.pl").
-:- consult("TDAsection_21254766_AninatNorambuena.pl").
-:- consult("TDAline_21254766_AninatNorambuena.pl").
 %TDA pcar
+
+carType(X):-
+    X == ct,
+    X == tr.
+
+% Constructor
+% RF9: pcar
+%
+% Descripcion:
+% Dominio:
+% Predicado:
+% Metas:
+% Submetas:
+% Clausulas:
+pcar(Id, Capacity, Model, Type, [Id, Capacity, Model, Type]):-
+    number(Id),
+    number(Capacity),
+    Capacity >= 0,
+    string(Model),
+    carType(Type).

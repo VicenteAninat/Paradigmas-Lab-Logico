27d9c63ec165c1f67281a2f1e1ca321d3d409dc7
Vicente Aninat <vicente.aninat@usach.cl>
2024-05-25 23:38:50 -0400

    Se añadio documentacion a los archivos de los TDA estacion y red de metro

--- TDAstation_21254766_AninatNorambuena.pl
+++ TDAstation_21254766_AninatNorambuena.pl
@@ -1,13 +1,15 @@
 :- module(tdastation_21254766_AninatNorambuena,
-         [station/1, encontrarEstacion/3, getStationId/2, getStationName/2, getStationType/2]).
+         [station/5, station/1, encontrarEstacion/3, getStationId/2, getStationName/2, getStationType/2]).
 %TDA station
-
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que define un tipo de estacion de metro de tipo
+ c (combinacion), m (mantencion), r (regular) o t (terminal)
+Dominio: X(string)
+Predicado: type(X)
+Metas: type/1
+Submetas:
+Clausulas:
+*/
 type(X):-
     X = "c";
     X = "m";
@@ -15,14 +17,16 @@ type(X):-
     X = "t".
 
 % Constructor
-% RF2: station
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF2: station
+
+Descripcion: Predicado constructor de una estacion de metro
+Dominio: Id(int), Name(string), Type(type), StopTime(int)>=0
+Predicado: station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime])
+Metas: station/5
+Submetas: number/1, string/1, type/1
+Clausulas:
+*/
 station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]):-
     number(Id),
     string(Name),
@@ -31,52 +35,63 @@ station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]):-
     StopTime >= 0.
 
 % Selectores
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae el ID de una estacion
+Dominio: Id(int)
+Predicado: getStationId([Id,_,_,_], Id)
+Metas: getStationId/2
+Submetas:
+Clausulas:
+*/
 getStationId([Id,_,_,_], Id).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae el ID de una estacion
+Dominio: Name(string)
+Predicado: getStationName([_,Name,_,_], Name)
+Metas: getStationName/2
+Submetas:
+Clausulas:
+*/
 getStationName([_,Name,_,_], Name).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae el ID de una estacion
+Dominio: Type(type)
+Predicado: getStationType([_,_,Type,_], Type)
+Metas: getStationType/2
+Submetas:
+Clausulas:
+*/
 getStationType([_,_,Type,_], Type).
 
 % Pertenencia
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que comprueba la validez de una estacion
+Dominio: Id(int), Name(string), Type(type), StopTime(int)>=0
+Predicado: station([Id, Name, Type, StopTime])
+Metas: station/1
+Submetas: number/1, string/1, type/1
+Clausulas:
+*/
 station([Id, Name, Type, StopTime]):-
     number(Id),
     string(Name),
     type(Type),
-    number(StopTime).
+    number(StopTime),
+    StopTime >= 0.
 
 % Predicados auxiliares
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Encuentra una estacion de una lista en base a su nombre
+Dominio: H(cabeza de lista), T(cola de lista), Nombre(string),
+Estacion(station)
+Predicado: encontrarEstacion([H|_], Nombre, Estacion)
+encontrarEstacion([H|T], Nombre, Estacion)
+Metas: encontrarEstacion/3
+Submetas: getStationName/2, encontrarEstacion/3(recursividad)
+Clausulas:
+*/
 encontrarEstacion([H|T], Nombre, Estacion):-
     getStationName(H, NombreEstacion),
     Nombre \= NombreEstacion,
--- TDAsubway_21254766_AninatNorambuena.pl
+++ TDAsubway_21254766_AninatNorambuena.pl
@@ -1,72 +1,87 @@
 :- module(tdasubway_21254766_AninatNorambuena,
-         [subway/3]).
+         [subway/3, subwayAddDriver/3, subwayAddLine/3, subwayAddTrain/3]).
 :- use_module("utilidades_21254766_AninatNorambuena").
-%TDA subway
+% TDA subway
 
 % Constructor
-% RF16: subway
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF16: subway
+
+Descripcion: Predicado que permite crear una red de metro.
+Dominio: Id(int), Nombre(string)
+Predicado: subway(Id, Nombre, [Id, Nombre, [], [], []])
+Metas: subway/1
+Submetas: number/1, string/1
+Clausulas:
+*/
 subway(Id, Nombre, [Id, Nombre, [], [], []]):-
     number(Id),
     string(Nombre).
 
 % Selectores
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae el Id de una red de metro
+Dominio: Id(int)
+Predicado: getSubwayId([Id,_,_,_,_], Id)
+Metas: getSubwayId/2
+Submetas:
+Clausulas:
+*/
 getSubwayId([Id,_,_,_,_], Id).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae el nombre de una red de metro
+Dominio: Nombre(string)
+Predicado: getSubwayNombre([_,Nombre,_,_,_], Nombre)
+Metas: getSubwayNombre/2
+Submetas:
+Clausulas:
+*/
 getSubwayNombre([_,Nombre,_,_,_], Nombre).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae los trenes de una red de metro
+Dominio: Trains(train list)
+Predicado: getSubwayTrain([_,_,Trains,_,_], Trains)
+Metas: getSubwayTrain/2
+Submetas:
+Clausulas:
+*/
 getSubwayTrain([_,_,Trains,_,_], Trains).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae las lineas de una red de metro
+Dominio: Lines(line list)
+Predicado: getSubwayLine([_,_,_,Lines,_], Lines)
+Metas: getSubwayLine/2
+Submetas:
+Clausulas:
+*/
 getSubwayLine([_,_,_,Lines,_], Lines).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae los conductores de una red de metro
+Dominio: Drivers(driver list)
+Predicado: getSubwayDriver([_,_,_,_,Drivers], Drivers)
+Metas: getSubwayDriver/2
+Submetas:
+Clausulas:
+*/
 getSubwayDriver([_,_,_,_,Drivers], Drivers).
 
+
 % Modificadores
-% RF17: subwayAddTrain
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF17: subwayAddTrain
+
+Descripcion: Predicado que permite añadir trenes a una red de metro.
+Dominio: Sub(subway), Trains(train list), SubwayOut(subway)
+Predicado: subwayAddTrain(Sub, Trains, SubwayOut)
+Metas: subwayAddTrain/3
+Submetas: getSubwayTrain/2, verificarAusenciaListas/2, append/3,
+getSubwayId/2, getSubwayNombre/2, getSubwayLine/2, getSubwayDriver/2
+Clausulas:
+*/
 subwayAddTrain(Sub, Trains, SubwayOut):-
     getSubwayTrain(Sub, SubwayTrains),
     verificarAusenciaListas(SubwayTrains, Trains),
@@ -77,14 +92,17 @@ subwayAddTrain(Sub, Trains, SubwayOut):-
     getSubwayDriver(Sub, SubDriver),
     SubwayOut = [SubId, SubNombre, NuevoSubwayTrains, SubLine, SubDriver].
 
-% RF18: subwayAddLine
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF18: subwayAddLine
+
+Descripcion: Predicado que permite añadir líneas a una red de metro.
+Dominio: Sub(subway), Lines(line list), SubwayOut(subway)
+Predicado: subwayAddLine(Sub, Lines, SubwayOut)
+Metas: subwayAddLine/3
+Submetas: getSubwayLine/2, verificarAusenciaListas/2, append/2,
+getSubwayId/2, getSubwayNombre/2, getSubwayTrain/2, getSubwayDriver/2
+Clausulas:
+*/
 subwayAddLine(Sub, Lines, SubwayOut):-
     getSubwayLine(Sub, SubwayLines),
     verificarAusenciaListas(SubwayLines, Lines),
@@ -95,14 +113,17 @@ subwayAddLine(Sub, Lines, SubwayOut):-
     getSubwayDriver(Sub, SubDriver),
     SubwayOut = [SubId, SubNombre, SubTrain, NuevoSubwayLines, SubDriver].
 
-% RF19: subwayAddDriver
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF19: subwayAddDriver
+
+Descripcion: Predicado que permite añadir conductores a una red de metro.
+Dominio: Sub(subway), Drivers(driver list), SubwayOut(subway)
+Predicado: subwayAddDriver(Sub, Drivers, SubwayOut)
+Metas: subwayAddDriver/3
+Submetas: getSubwayDriver/2, verificarAusenciaListas/2, append/3,
+getSubwayId/2, getSubwayNombre/2, getSubwayTrain/2, getSubwayLine/2
+Clausulas:
+*/
 subwayAddDriver(Sub, Drivers, SubwayOut):-
     getSubwayDriver(Sub, SubwayDrivers),
     verificarAusenciaListas(SubwayDrivers, Drivers),
@@ -113,13 +134,15 @@ subwayAddDriver(Sub, Drivers, SubwayOut):-
     getSubwayLine(Sub, SubLine),
     SubwayOut = [SubId, SubNombre, SubTrain, SubLine, NuevoSubwayDrivers].
 
-% RF20: subwayToString
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF20: subwayToString
+
+Descripcion: Función que permite expresar una red de metro en un formato String.
+Dominio: Sub(subway), StrOut(string)
+Predicado: subwayToString(Sub, StrOut)
+Metas: subwayToString/2
+Submetas:
+Clausulas:
+*/
 subwayToString(Sub, StrOut):-
     atomics_to_string(Sub, StrOut).

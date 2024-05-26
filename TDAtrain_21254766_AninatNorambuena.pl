793596718153d9f59cf420b78b44ea24eeb26996
Vicente Aninat <vicente.aninat@usach.cl>
2024-05-25 23:36:49 -0400

    Se agregó documentacion a los archivos de utilidades y el TDA tren

--- TDAtrain_21254766_AninatNorambuena.pl
+++ TDAtrain_21254766_AninatNorambuena.pl
@@ -2,17 +2,20 @@
          [train/6, trainAddCar/4, trainRemoveCar/3, isTrain/1, trainCapacity/2]).
 :- use_module(utilidades_21254766_AninatNorambuena).
 :- use_module(tdapcar_21254766_AninatNorambuena).
-%TDA train
+% TDA train
 
 % Constructor
-% RF10: train
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF10: train
+
+Descripcion: Predicado que permite crear un tren o convoy.
+Dominio: id(int), Maker(string), RailType(string), Speed(int),
+Pcars(pcar list)
+Predicado: train(Id, Maker, RailType, Speed, Pcars, [Id, Maker, RailType, Speed, Pcars])
+Metas: train/6
+Submetas: number/1, string/1
+Clausulas:
+*/
 train(Id, Maker, RailType, Speed, Pcars, [Id, Maker, RailType, Speed, Pcars]):-
     number(Id),
     string(Maker),
@@ -21,56 +24,69 @@ train(Id, Maker, RailType, Speed, Pcars, [Id, Maker, RailType, Speed, Pcars]):-
     Speed >= 0.
 
 % Selectores
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae el Id de un tren
+Dominio: Id(int)
+Predicado: getTrainId([Id,_,_,_,_], Id)
+Metas: getTrainId/2
+Submetas:
+Clausulas:
+*/
 getTrainId([Id,_,_,_,_], Id).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae el maker de un tren
+Dominio: Maker(string)
+Predicado: getTrainMaker([_,Maker,_,_,_], Maker)
+Metas: getTrainMaker/2
+Submetas:
+Clausulas:
+*/
 getTrainMaker([_,Maker,_,_,_], Maker).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae el tipo de rail de un tren
+Dominio: RailType(string)
+Predicado: getTrainRailType([_,_,RailType,_,_], RailType)
+Metas: getTrainRailType/2
+Submetas:
+Clausulas:
+*/
 getTrainRailType([_,_,RailType,_,_], RailType).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae la velocidad de un tren
+Dominio: Speed(int)
+Predicado: getTrainSpeed([_,_,_,Speed,_], Speed)
+Metas: getTrainSpeed/2
+Submetas:
+Clausulas:
+*/
 getTrainSpeed([_,_,_,Speed,_], Speed).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que extrae los carros de un tren
+Dominio: Pcars(pcar list)
+Predicado: getTrainPcar([_,_,_,_,Pcars], Pcars)
+Metas: getTrainPcar/2
+Submetas:
+Clausulas:
+*/
 getTrainPcar([_,_,_,_,Pcars], Pcars).
 
 % Modificadores
-% RF11: trainAddCar
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF11: trainAddCar
+
+Descripcion: Función que permite añadir carros a un tren en una posición dada.
+Dominio: Train(train), Pcar(pcar), Position(int), TrainOut(train)
+Predicado: trainAddCar(Train, Pcar, Position, TrainOut)
+Metas: trainAddCar/4
+Submetas: number/1, getTrainPcar/2, verificarAusencia/2,
+verificarPosicion/2, agregarElemListaIndice/4, getTrainId/2,
+getTrainMaker/2, getTrainRailType/2, getTrainSpeed/2
+Clausulas:
+*/
 trainAddCar(Train, Pcar, Position, TrainOut):-
     number(Position),
     Position >= 0,
@@ -84,14 +100,18 @@ trainAddCar(Train, Pcar, Position, TrainOut):-
     getTrainSpeed(Train, TrainSpeed),
     TrainOut = [TrainId, TrainMaker, TrainRailType, TrainSpeed, NuevaListaPcar].
 
-% RF12: trainRemoveCar
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF12: trainRemoveCar
+
+Descripcion: Predicado que permite eliminar un carro desde el convoy.
+Dominio: Train(train), Position(int), TrainOut(train)
+Predicado: trainRemoveCar(Train, Position, TrainOut)
+Metas: trainRemoveCar/3
+Submetas: number/1, getTrainPcar/2, verificarAusencia/2,
+verificarPosicion/2, eliminarElemListaIndice/3, getTrainId/2,
+getTrainMaker/2, getTrainRailType/2, getTrainSpeed/2
+Clausulas:
+*/
 trainRemoveCar(Train, Position, TrainOut):-
     number(Position),
     Position >= 0,
@@ -105,14 +125,21 @@ trainRemoveCar(Train, Position, TrainOut):-
     TrainOut = [TrainId, TrainMaker, TrainRailType, TrainSpeed, NuevaListaPcar].
 
 % Pertenencia
-% RF13: isTrain
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF13: isTrain
+
+Descripcion: Predicado que permite determinar si un elemento es un tren
+válido, esto es, si el elemento tiene la estructura de tren y los carros
+que lo conforman son compatibles (mismo modelo) y tienen una disposición
+coherente con carros terminales (tr) en los extremos y centrales (ct) en
+medio del convoy.
+Dominio: Train(train)
+Predicado: isTrain(Train)
+Metas: isTrain/1
+Submetas: getTrainId/2, getTrainMaker/2, getTrainRailType/2,
+getTrainSpeed/2, getTrainPcar/2, number/1, string/1
+Clausulas:
+*/
 isTrain(Train):-
     getTrainId(Train, TrainId),
     getTrainMaker(Train, TrainMaker),
@@ -127,27 +154,32 @@ isTrain(Train):-
     carrosValidos(TrainPcars).
 
 % Auxiliares
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que calcula la capacidad total de una lista de
+carros
+Dominio: H(cabeza de lista), T(cola de lista), Capacidad(int)
+Predicado: capacidadTren([],0)
+capacidadTren([H|T], Capacidad)
+Metas: capacidadTren/2
+Submetas: getPcarCapacity/2, capacidadTren/2(recursividad)
+Clausulas:
+*/
 capacidadTren([],0).
 capacidadTren([H|T], Capacidad):-
     getPcarCapacity(H, Cap1),
     capacidadTren(T, Cap2),
     Capacidad is Cap1 + Cap2.
 
-% RF14: trainCapacity
-%
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+RF14: trainCapacity
+
+Descripcion: Predicado que permite determinar la capacidad máxima de pasajeros del tren.
+Dominio: Train(train), Capacity(int)
+Predicado: trainCapacity(Train, Capacity)
+Metas: trainCapacity/2
+Submetas: getTrainPcar/2, capacidadTren/2
+Clausulas:
+*/
 trainCapacity(Train, Capacity):-
     getTrainPcar(Train, TrainPcars),
     capacidadTren(TrainPcars, Capacity).
--- utilidades_21254766_AninatNorambuena.pl
+++ utilidades_21254766_AninatNorambuena.pl
@@ -2,30 +2,42 @@
          [deleteDuplicate/2, cortarListaInferior/3, cortarListaSuperior/3, listAdd/3, verificarAusencia/2, verificarAusenciaListas/2, cortarExtremos/2, verificarPosicion/2, agregarElemListaIndice/4, eliminarElemListaIndice/3]).
 % Utilidades
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que filtra un elemento de una lista
+Dominio: X(elemento de lista), Y(Elemento de lista), Elem(elemento de
+lista), Lista(list), R(list)
+Predicado: objetivo(X, Y)
+filtrar(Elem, Lista, R)
+Metas: objetivo/2, filtrar/3
+Submetas: exclude/3
+Clausulas:
+*/
 objetivo(X, Y):- X = Y.
 filtrar(Elem, Lista, R):- exclude(objetivo(Elem), Lista, R).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que devuelve una lista sin elementos repetidos
+Dominio: H(Cabeza de lista), L(cola de lista), R(list)
+Predicado: deleteDuplicate([],[])
+deleteDuplicate([H|L], R)
+Metas: deleteDuplicate/2
+Submetas: filtrar/3, deleteDuplicate/2(recursividad)
+Clausulas:
+*/
 deleteDuplicate([],[]). %Caso base
 deleteDuplicate([H|L], R):- filtrar(H, L, LFiltrada), R=[H|Y], deleteDuplicate(LFiltrada, Y).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que corta una lista a partir de un elemento
+Dominio: H(cabeza de lista), T(cola de lista), Elem(Elemento de lista),
+NuevaLista(list)
+Predicado: cortarListaInferior([], _, [])
+cortarListaInferior([H|T], Elem, NuevaLista)
+cortarListaInferior([H|T], Elem, NuevaLista)
+Metas: cortarListaInferior/3
+Submetas: cortarListaInferior/3(recursividad)
+Clausulas:
+*/
 cortarListaInferior([], _, []).
 cortarListaInferior([H|T], Elem, NuevaLista):-
     H \= Elem,
@@ -34,12 +46,17 @@ cortarListaInferior([H|T], Elem, NuevaLista):-
     H = Elem,
     NuevaLista = [H|T].
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que corta una lista hasta un elemento
+Dominio: H(cabeza de lista), T(cola de lista), Elem(Elemento de lista),
+NuevaLista(list)
+Predicado: cortarListaSuperior([], _, [])
+cortarListaSuperior([H|T], Elem, NuevaLista)
+cortarListaSuperior([H|_], Elem, NuevaLista)
+Metas: cortarListaSuperior/3
+Submetas: cortarListaSuperior/3(recursividad)
+Clausulas:
+*/
 cortarListaSuperior([], _, []).
 cortarListaSuperior([H|T], Elem, NuevaLista):-
     H \= Elem,
@@ -49,46 +66,62 @@ cortarListaSuperior([H|_], Elem, NuevaLista):-
     H = Elem,
     NuevaLista = [H].
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que agrega un elemento a una lista
+Dominio: H(cabeza de lista), T(cola de lista), Elem(elemento de lista),
+NuevaLista(list)
+Predicado: listAdd([],Elem, [Elem])
+listAdd([H|T], Elem, NuevaLista)
+Metas: listAdd/3
+Submetas: listAdd/3(recursividad)
+Clausulas:
+*/
 listAdd([],Elem, [Elem]).
 listAdd([H|T], Elem, NuevaLista):-
     listAdd(T, Elem, NuevaLista2),
     NuevaLista = [H, NuevaLista2].
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que comprueba la ausencia de un elemento en una
+lista
+Dominio: H(cabeza de lista), T(cola de lista), Elem(elemento de lista)
+Predicado: verificarAusencia([],_)
+verificarAusencia([H|T], Elem)
+Metas: verificarAusencia/2
+Submetas: verificarAusencia/2(recursividad)
+Clausulas:
+*/
 verificarAusencia([],_).
 verificarAusencia([H|T], Elem):-
     H \= Elem,
     verificarAusencia(T, Elem).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que comprueba que ningun elemento de una lista se
+repita en otra
+Dominio: List(list), H(cabeza de lista), T(cola de lista)
+Predicado: verificarAusenciaListas(_,[])
+verificarAusenciaListas([],_)
+verificarAusenciaListas(List, [H|T])
+Metas: verificarAusenciaListas/2
+Submetas: member/2, verificarAusenciaListas/2(recursividad)
+Clausulas:
+*/
 verificarAusenciaListas(_,[]).
 verificarAusenciaListas([],_).
 verificarAusenciaListas(List, [H|T]):-
     \+ member(H, List),
     verificarAusenciaListas(List, T).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que es verdadero si la lista resultante es la
+primera lista sin el elemento final ni el inicial
+Dominio: H(cabeza de lista), T(cola de lista), ListaCortada(list)
+Predicado: cortarExtremos([H|T], ListaCortada)
+Metas: cortarExtremos/2
+Submetas: last/2, select/3, reverse/2
+Clausulas:
+*/
 cortarExtremos([H|T], ListaCortada):-
     Primero = H,
     last([H|T], Ultimo),
@@ -97,22 +130,30 @@ cortarExtremos([H|T], ListaCortada):-
     select(Ultimo, ListaInv, ListaFinalInv),
     reverse(ListaFinalInv, ListaCortada).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que es verdadero si una posicion es accesible
+dentro de una lista
+Dominio: Lista(list), Posicion(int)
+Predicado: verificarPosicion(Lista, Posicion)
+Metas: verificarPosicion/2
+Submetas: length/2
+Clausulas:
+*/
 verificarPosicion(Lista, Posicion):-
     length(Lista, X),
     Posicion =< X.
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que es verdadero si la lista final es la primera
+lista con un nuevo elemento agregado en una posicion especifica
+Dominio: Lista(list), Elem(elemento de lista), Indice(int),
+NuevaLista(list)
+Predicado: agregarElemListaIndice(Lista, Elem, Indice, NuevaLista)
+agregarElemListaIndice([H|T], Elem, Indice, NuevaLista)
+Metas: agregarElemListaIndice/4
+Submetas: append/3, agregarElemListaIndice/4(recursividad)
+Clausulas:
+*/
 agregarElemListaIndice(Lista, Elem, Indice, NuevaLista):-
     Indice = 0,
     ListaElem = [Elem],
@@ -124,11 +165,14 @@ agregarElemListaIndice([H|T], Elem, Indice, NuevaLista):-
     agregarElemListaIndice(T, Elem, Indice2, Lista2),
     append([H], Lista2, NuevaLista).
 
-% Descripcion:
-% Dominio:
-% Predicado:
-% Metas:
-% Submetas:
-% Clausulas:
+/*
+Descripcion: Predicado que es verdadero si la lista final es la primera
+lista sin el elemento de una posicion especifica
+Dominio: Lista(list), Indice(int), NuevaLista(list)
+Predicado: eliminarElemListaIndice(Lista, Indice, NuevaLista)
+Metas: eliminarElemListaIndice/3
+Submetas: nth0/4
+Clausulas:
+*/
 eliminarElemListaIndice(Lista, Indice, NuevaLista):-
     nth0(Indice, Lista, _, NuevaLista).
